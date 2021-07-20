c7f31c6e19b240644e5d3e917e522df25681bda0
mbdx4srt <35216966+mbdx4srt@users.noreply.github.com>
2021-07-20 12:32:37 +0100

    Added ID get

--- app.js
+++ app.js
@@ -13,11 +13,18 @@ app.get("/airports", (req, res) => {
 app.get("/airports/:id", (req, res) => {
     id = req.params.id
     const result = airports.find(airport => airport.icao == id);
-
     res.json(result);
 
 });
 
+app.put("/airports/:id", (req, res) => {
+    id = req.params.id
+    var result = airports.find(airport => airport.icao == id);
+    result = req.body
+    console.log(result)
+    res.json(result);
+    res.sendStatus(200);
+});
 
 
 app.post("/airports", (req, res) => {
